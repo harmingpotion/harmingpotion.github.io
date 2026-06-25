352e3564e3357d4ecbdec73e35fffcdedbf6ee8f
Harming Potion <risseeuw.harm@gmail.com>
2026-06-25 12:35:04 +0200

    move items into root directory

--- public/js/service-worker.js
+++
@@ -1,8 +0,0 @@
-
-self.addEventListener('install', (event) => {
-  console.log('Service Worker: Geïnstalleerd');
-});
-
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker: Geactiveerd');
-});
